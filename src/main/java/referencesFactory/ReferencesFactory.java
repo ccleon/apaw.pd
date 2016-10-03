88966e78c57b967fd92d4ec1ee26c4a5deda0196
Cori <Cori@138.4.199.198>
2016-10-03 10:39:23 +0200

    ReferencesFactory

---
+++ src/main/java/referencesFactory/ReferencesFactory.java
@@ -0,0 +1,41 @@
+package referencesFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ReferencesFactory {
+	private static ReferencesFactory referencesFactory;
+    
+	private Map<String, Integer> references;
+
+    private int reference;
+
+    public ReferencesFactory() {
+        this.references = new HashMap<>();
+        this.reference = 0;
+    }
+    
+    public static ReferencesFactory getFactory(){
+    	if (referencesFactory ==null){
+    		referencesFactory = new ReferencesFactory();
+    	};
+    	return referencesFactory;
+    }
+
+    public int getReference(String key) {
+        Integer result = this.references.get(key);
+        if (result == null) {
+            this.references.put(key, this.reference);
+            result = this.reference;
+            reference++;
+        }
+        return result;
+    }
+
+    public void removeReference(String key) {
+        this.references.remove(key);
+    }
+
+}
+
+
